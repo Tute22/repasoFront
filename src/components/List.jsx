1e8ee4639ebeb58f3010006c765073c672aecf5e
Tute22 <agustinsandoval1601@gmail.com>
2023-08-24 10:53:40 -0300

    pokedex

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,33 +1,47 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const List = () => {
-    const [list,setList] = useState([])
-    
-    useEffect(()=>{
-        axios("https://pokeapi.co/api/v2/pokemon?limit=30&offset=15").then(({data})=>{
-            Promise.all(data.results.map(res=>{
-                return axios(res.url).then(e=> { 
-                  return {url: e.data.sprites.other.dream_world.front_default, name: res.name, id: e.data.id}})
-            })).then(data=>setList(data))
-           
-        })
-    },[])
+  const [list, setList] = useState([]);
 
-    console.log(list);
+  useEffect(() => {
+    axios("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(
+      ({ data }) => {
+        Promise.all(
+          data.results.map((res) => {
+            return axios(res.url).then((e) => {
+              return {
+                url: e.data.sprites.other["official-artwork"].front_default,
+                name: res.name,
+                id: e.data.id,
+              };
+            });
+          })
+        ).then((data) => setList(data));
+      }
+    );
+  }, []);
+
+  console.log(list);
   return (
-    <div style={{backgroundColor:"aliceblue", display:"flex", flexWrap:"wrap"}}>
-        {list.map((element,idx)=>{
-            
-        return <div key={idx} className="card">
-                    <Link to={`/pokemon/${element.id}`}>
-                        <img src={element.url} alt="Img" />
-                    </Link>
-                    <span>{element.name}</span>
-                </div>
-})}
+    <div
+      style={{
+        backgroundColor: "aliceblue",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {list.map((element, idx) => {
+        return (
+          <div key={idx} className="card">
+            <Link to={`/pokemon/${element.id}`}>
+              <img src={element.url} alt="Img" />
+            </Link>
+            <span>{element.name}</span>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
